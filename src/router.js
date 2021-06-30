b206bee0b088cc2e8d804a851a9a3ba7881e719c
RyosukeSomeya <rx.78.0618@gmail.com>
2021-07-01 01:44:55 +0900

    ダッシュボードページ実装,ユーザー情報表示の実装

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Signup from './components/Siginup';
 import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 Vue.use(Router);
 
@@ -17,6 +18,12 @@ export default new Router({
             path: '/signup',
             name: 'signup',
             component: Signup
-        }
+        },
+        {
+            path: '/dashboard/:id',
+            name: 'dashboard',
+            component: Dashboard,
+            props: true
+        },
     ]
 })
